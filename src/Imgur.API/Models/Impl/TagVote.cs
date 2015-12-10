09d22844dc51fdd6aeba6ddec0a90d231a71e871
Lars Bokkers <lars@larsbokkers.net>
2015-12-10 15:57:37 +0100

    Added implementation of TagVote.

---
+++ src/Imgur.API/Models/Impl/TagVote.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: lbokkers
+ * Date: 10-12-2015
+ * Time: 3:54 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Imgur.API.Models.Impl
+{
+	/// <summary>
+	/// Description of TagVote.
+	/// </summary>
+	public class TagVote : ITagVote
+	{
+		public TagVote()
+		{
+		}
+
+		#region ITagVote implementation
+
+		[JsonProperty("ups")]
+		public int Ups {get;set;}
+
+		[JsonProperty("downs")]
+		public int Downs {get;set;}
+
+		[JsonProperty("name")]
+		public string Name {get;set;}
+
+		[JsonProperty("author")]
+		public string Author {get;set;}
+		#endregion
+	}
+}
