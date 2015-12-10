d56cb9804acf79c90a78eb8e763dd0d3ea4e83a4
Lars Bokkers <lars@larsbokkers.net>
2015-12-10 16:04:37 +0100

    Added implementation of Vote.

---
+++ src/Imgur.API/Models/Impl/Vote.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: lbokkers
+ * Date: 10-12-2015
+ * Time: 4:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Imgur.API.Models.Impl
+{
+	/// <summary>
+	/// Description of Vote.
+	/// </summary>
+	public class Vote : IVote
+	{
+		public Vote()
+		{
+		}
+
+		#region IVote implementation
+		[JsonProperty("ups")]
+		public int Ups {get;set;}
+
+		[JsonProperty("downs")]
+		public int Downs {get;set;}
+
+		#endregion
+	}
+}
