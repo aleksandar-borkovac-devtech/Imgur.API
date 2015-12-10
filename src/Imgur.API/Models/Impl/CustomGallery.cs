f31c9586559a8959a7e3cffea69feaab83abb0c5
Lars Bokkers <lars@larsbokkers.net>
2015-12-10 16:13:31 +0100

    Added implementation of CustomGallery and MemeMetaData.

---
+++ src/Imgur.API/Models/Impl/CustomGallery.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: lbokkers
+ * Date: 10-12-2015
+ * Time: 4:11 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Imgur.API.Models.Impl
+{
+	/// <summary>
+	/// Description of CustomGallery.
+	/// </summary>
+	public class CustomGallery : ICustomGallery
+	{
+		public CustomGallery()
+		{
+		}
+
+		#region ICustomGallery implementation
+
+		[JsonProperty("account_url")]
+		public string AccountUrl {get;set;}
+
+		[JsonProperty("link")]
+		public string Link {get;set;}
+
+		[JsonProperty("tags")]
+		public string[] Tags {get;set;}
+
+		[JsonProperty("item_count")]
+		public int ItemCount {get;set;}
+
+		[JsonProperty("items")]
+		public IGalleryAlbumImageBase[] Items {get;set;}
+
+		#endregion
+	}
+}
---
+++ src/Imgur.API/Models/Impl/MemeMetaData.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: lbokkers
+ * Date: 10-12-2015
+ * Time: 4:13 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Imgur.API.Models.Impl
+{
+	/// <summary>
+	/// Description of MemeMetadata.
+	/// </summary>
+	public class MemeMetaData : IMemeMetaData
+	{
+		public MemeMetaData()
+		{
+		}
+
+		#region IMemeMetaData implementation
+
+		[JsonProperty("meme_name")]
+		public string MemeName {get;set;}
+
+		[JsonProperty("top_text")]
+		public string TopText {get;set;}
+
+		[JsonProperty("bottom_text")]
+		public string BottomText {get;set;}
+
+		[JsonProperty("bg_image")]
+		public string BgImage {get;set;}
+
+		#endregion
+	}
+}
