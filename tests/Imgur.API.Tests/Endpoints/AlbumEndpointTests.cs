a080f29b9fa016151b4ffeb87ff84da095bcda7b
Lars Bokkers <LBokkers@kpnmail.nl>
2015-12-11 01:01:26 +0100

    Started writing tests for album endpoint.

--- tests/Imgur.API.Tests/Endpoints/AlbumEndpointTests.cs
+++ tests/Imgur.API.Tests/Endpoints/AlbumEndpointTests.cs
@@ -1,6 +1,31 @@
-﻿namespace Imgur.API.Tests.Endpoints
+﻿using Imgur.API.Authentication.Impl;
+using Imgur.API.Endpoints;
+using Imgur.API.Endpoints.Impl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Threading.Tasks;
+
+namespace Imgur.API.Tests.Endpoints
 {
-    internal class AlbumEndpointTests
+    [TestClass]
+    public class AlbumEndpointTests
     {
+        [TestMethod]
+        public void GetAlbumAsync_WithId_ReceivedIsTrue()
+        {
+            var endpoint = Substitute.For<IAlbumEndpoint>();
+            endpoint.GetAlbumAsync("1234");
+            endpoint.Received().GetAlbumAsync("1234");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task GetAlbumAsync_WithNullId_ThrowsArgumentNullException()
+        {
+            var imgurAuth = new ImgurClient("123", "1234");
+            var endpoint = new AlbumEndpoint(imgurAuth);
+            await endpoint.GetAlbumAsync(null);
+        }
     }
 }
